dbbf4656382ade32621a3bd03885313f94ef08a3
ShTair <git@vsh.jp>
2020-05-16 13:28:25 +0900

    実装

--- BlobLayerChanger/Program.cs
+++ BlobLayerChanger/Program.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Blob;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace BlobLayerChanger
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var account = CloudStorageAccount.Parse(args[0]);
+            var client = account.CreateCloudBlobClient();
+
+            var container = client.GetContainerReference(args[1]);
+            var blobs = container.ListBlobs(args[2], true).Cast<CloudBlockBlob>();
+            foreach (var blob in blobs)
+            {
+                Console.Write($"{blob.Name}");
+                var tier = blob.Properties.StandardBlobTier;
+                if (tier == StandardBlobTier.Hot)
+                {
+                    Console.Write($"...");
+                    await blob.SetStandardBlobTierAsync(StandardBlobTier.Archive);
+                    Console.Write($"OK");
+                }
+
+                Console.WriteLine();
+            }
         }
     }
 }
